db9819af04d65bab7281b8467891c9c707b0b61e
gentanaka <gen.tanaka2001@gmail.com>
2023-04-05 08:38:09 +0900

    2023/04/05 practice

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card/Card";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+    const initialUrl = "https://pokeapi.co/api/v2/pokemon";
+    const [loading, setLoading] = useState(true);
+    const [pokemonData, setPokemonData] = useState([]);
+
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            // 全てのポケモンデータを取得
+            let res = await getAllPokemon(initialUrl);
+            // 各ポケモンの詳細なデータを取得
+            loadPokemon(res.results);
+            setLoading(false);
+        };
+        fetchPokemonData();
+    }, []);
+
+    const loadPokemon = async (data) => {
+        let _pokemonData = await Promise.all(
+            data.map((pokemon) => {
+                let pokemonRecord = getPokemon(pokemon.url);
+                return pokemonRecord;
+            })
+        );
+        setPokemonData(_pokemonData);
+    };
+
+    return (
+        <>
+            <Navbar />
+            <div className="App">
+                {loading ? (
+                    <h1>ロード中・・・</h1>
+                ) : (
+                    <>
+                        <div className="pokemonCardContainer">
+                            {pokemonData.map((pokemon, i) => {
+                                return <Card key={i} pokemon={pokemon} />
+                            })}
+                        </div>
+                    </>
+                )}
+            </div>
+        </>
+    );
+}
+
+export default App;
